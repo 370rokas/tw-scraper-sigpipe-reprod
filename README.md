d24fb0cfa78fde87b3d7b283600301e72205ba31
Rokas <0x16dev@gmail.com>
2025-01-23 16:58:07 +0200

    Commit reprod code & docker stuff

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tw-scraper-sigpipe-reprod
  
+Reproduction for https://github.com/mnwato/tradingview-scraper/issues/19
+
+Run dockerised version with:
+```
+docker build -t tw-scraper-sigpipe-reprod . && docker run -it --rm --name sigpipe-reprod tw-scraper-sigpipe-reprod
+```
---
+++ app.py
@@ -0,0 +1,19 @@
+from tradingview_scraper.symbols.stream.price import RealTimeData
+import threading, time
+
+EXCHANGE_SYMBOLS = ["FOREXCOM:XAUUSD", "FOREXCOM:EURUSD", "FOREXCOM:GBPJPY"]
+
+cache = {}
+
+def process_packet(packet):
+    print("Processing packet:", packet.get('m'))
+    # Simulate the processing of the packet
+    time.sleep(1)
+
+if __name__ == "__main__":
+    real_time_data = RealTimeData()
+    data_generator = real_time_data.get_latest_trade_info(exchange_symbol=EXCHANGE_SYMBOLS)
+
+    for packet in data_generator:
+        # Send off the packet to be handled in a thread
+        threading.Thread(target=process_packet, args=(packet,)).start()
